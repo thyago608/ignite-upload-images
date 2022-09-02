1bff96989bb737eaa26a806c7ed7964eafaf35e3
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-09-02 10:12:20 -0400

    Added Fetch and Button More

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,17 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface PageProps {
+  data: {
+    title: string;
+    description: string;
+    url: string;
+    ts: number;
+    id: string;
+  }[];
+  after: string;
+}
+
 export default function Home(): JSX.Element {
   const {
     data,
@@ -17,19 +28,33 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    ['images'],
+    async ({ pageParam = null }) => {
+      const response = await api.get('images', {
+        params: { after: pageParam },
+      });
+      return response.data;
+    },
+    {
+      getNextPageParam: lastPage => lastPage.nextCursor,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const pageList = data?.pages.map((page: PageProps) => {
+      return [...page.data];
+    });
+
+    return pageList?.flat();
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +62,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
